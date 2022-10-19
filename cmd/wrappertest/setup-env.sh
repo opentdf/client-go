a0a79795fc25c96a9f39882992d2ab9bdc4b18a9
Ben Leggett <854255+bleggett@users.noreply.github.com>
2022-10-19 17:00:07 -0400

    Add GetTDFStorageTypeDescriptor punch-thru (#14)
    * Add GetTDFStorageTypeDescriptor punch-thru
    
    * twiddle
    
    * Add this
    
    * Do this
    
    * Sync with `client-cpp` 1.3.2

--- cmd/wrappertest/setup-env.sh
+++ cmd/wrappertest/setup-env.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 export TDF_USER="user1@nope.com"
 export TDF_CLIENTID="tdf-client"
-export TDF_KAS_URL="http://localhost:65030"
-export TDF_OIDC_URL="http://localhost:65018"
+export TDF_KAS_URL="http://localhost:8000"
+export TDF_OIDC_URL="http://localhost:51715"
 export TDF_ORGNAME="tdf"
 export TDF_CLIENTSECRET="123-456"
 export TDF_EXTERNALTOKEN=""
